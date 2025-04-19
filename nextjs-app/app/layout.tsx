22fc2c758d17535cd5b0c07c7f4ed195e963a8d8
Kye Browning <kye.browning@gmail.com>
2025-04-19 06:49:40 -0500

    remove navigation bar

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -79,7 +79,7 @@ export default async function RootLayout({
           )}
           {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
           <SanityLive onError={handleError} />
-          <Header />
+          {/* <Header /> */}
           <main className="">{children}</main>
           {/* <Footer /> */}
         </section>
